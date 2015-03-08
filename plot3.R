1952c85a82c22babf8ac8a1687e322bd88db0062
ronrest <roger.mirble@gmail.com>
2015-03-09 08:27:13 +1100

    Add comments to R files

--- plot3.R
+++ plot3.R
@@ -11,6 +11,16 @@ endDate <- strptime("2007-02-02 23:59:0", format="%Y-%m-%d %H:%M:%S")
 ################################################################################
 #                                                                    IMPORT DATA
 ################################################################################
+# Since the process of extracting the relevant rows of data from the data file  
+# is something that is shared by all of the plotN.R scripts, this code has been 
+# placed in a separate R file called "getData.R"
+# This was done to make the code recyclable, and easily editable from one single 
+# location. 
+# 
+# Its just a matter of calling the getDataInDateRange() function, and telling it 
+# where the data file is located, and specifying the start and end date-times of 
+# interest. 
+
 source("getData.R")
 df <- getDataInDateRange(dataFile, startDate, endDate)
 
